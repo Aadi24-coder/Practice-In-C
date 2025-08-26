661fca282e6a8489d7d5e7a6c033c99ac37429c1
Aadi Agarwal <136228683+Aadi24-coder@users.noreply.github.com>
2025-08-26 17:32:24 +0530

    Create myname.c
    print the age and name of person

---
+++ myname.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char name[100];
+    int age;
+    printf(" enter the name of the person");
+    scanf("%s",&name);
+    printf(" enter the age");
+    scanf("%d",&age);
+    return 0;
+}
