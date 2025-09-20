018b8faf841c5a87c4c96966e8ab46187df06d4a
Aadi Agarwal <136228683+Aadi24-coder@users.noreply.github.com>
2025-09-20 09:59:53 +0530

    add two matrices
    prints the sum of two matrices

--- arrays/sum_array.c
+++ arrays/sum_array.c
@@ -1,13 +1,70 @@
 #include <stdio.h>
 int main()
 {
-    int array[]={5,6,7,4,9,14,56};
-    int sum=0;
-    int size=sizeof(array)/sizeof(array[0]);
-    for(int i =0;i<size;i++)
+    int sum[100][100];
+    int a[100][100]; // matrix 1 
+    int b[100][100]; // matrix 2
+    int p,q,m,n;
+    printf(" enter number of rows and coloums for the first matrix(p,q)\n");
+    scanf("%d %d", &p , &q);
+    printf(" enter number of rows and coloums for the second matrix(m,n)\n");
+    scanf("%d %d", &m,&n);
+    printf("Enter elements of first matrix (%d x %d):\n", p, q);
+    for(int i=0;i<p;i++)
     {
-        sum+=array[i];
+        for(int j =0;j<q;j++)
+        {
+            scanf("%d",&a[i][j]);
+                }
     }
-    printf(" the sum of array elements is :%d\n" , sum);
+    printf("Enter elements of first matrix (%d x %d):\n", p, q);
+    for(int i=0;i<m;i++)
+    {
+        for(int j =0;j<n;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+
+
+    printf(" matrix 1 is  :\n");
+
+    for(int i=0;i<p;i++)
+    {
+        for(int j =0;j<q;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf(" matrix 1 is  :\n");
+    for(int i=0;i<m;i++)
+    {
+        for(int j =0;j<n;j++)
+        {
+            printf("%d ", b[i][j]);
+        }
+        printf("\n");
+    }
+    for(int i =0;i<p;i++)
+    {
+        for(int j=0;j<q;j++)
+        {
+            sum[i][j]=a[i][j]+b[i][j];
+        }
+    }
+    printf(" sum of two matrices are:\n");
+    for(int i =0;i<p;i++)
+    {
+        for(int j=0;j<q;j++)
+        {
+            printf("%4d",sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    
+
     return 0;
 }
